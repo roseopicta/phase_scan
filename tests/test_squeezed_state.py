7dd61fc4da1536de5dbd008dcdb260598bca436c
burlemarxiste <emilie.o.gillet+github@gmail.com>
2025-08-18 07:48:49 +0200

    One more unit test

--- tests/test_squeezed_state.py
+++ tests/test_squeezed_state.py
@@ -30,6 +30,7 @@ from phase_scan.ml_estimation import (
 
 from functools import partial
 import numpy as np
+import scipy as sp
 
 
 @pytest.mark.parametrize(
@@ -50,6 +51,32 @@ def test_squeezed_state(estimation_fn):
 
     np.random.seed(0)
     angles = np.linspace(0, 1, 100) * np.pi
+    samples = gaussian_utils.generate_scanned_samples(
+        sigma, angles, gaussian_utils.generate_samples_parallel, 1000
+    )
+    sigma_hat = estimation_fn(samples)
+    np.testing.assert_allclose(sigma, sigma_hat, atol=1e-2, rtol=1e-2)
+
+
+@pytest.mark.parametrize(
+    "estimation_fn",
+    [
+        ml_covariance_estimation_direct_ls,
+    ],
+)
+def test_two_independent_modes(estimation_fn):
+    squeezing_dB = 6
+    squeezing_angle = np.pi / 4
+    squeezing_s = 10 ** (-squeezing_dB / 10)
+
+    sigma = gaussian_utils.squeezed_vacuum(squeezing_s)
+    R_1 = gaussian_utils.rotation_symplectic(squeezing_angle)
+    R_2 = gaussian_utils.rotation_symplectic(squeezing_angle + np.pi / 2)
+    sigma = sp.linalg.block_diag(R_1 @ sigma @ R_1.T, R_2 @ sigma @ R_2.T)
+
+    np.random.seed(100)
+    angles = np.tile(np.linspace(0, 1, 1000) * np.pi, (2, 1)).T
+    print(angles.shape)
     samples = gaussian_utils.generate_scanned_samples(
         sigma, angles, gaussian_utils.generate_samples_parallel, 1000
     )
